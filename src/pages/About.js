b2638f222a3dce6f57ef9bc65f732977290af16d
Dv <dv@Dvs-MacBook-Pro.local>
2021-01-19 21:31:06 +0530

    react cocktail api

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+        voluptatibus perferendis excepturi beatae ea quibusdam maxime a, modi
+        quod doloremque natus, aliquam corporis provident eos incidunt omnis
+        voluptate minima odio reprehenderit facere similique veniam totam! Ab
+        corrupti voluptatem doloribus nulla!
+      </p>
+    </section>
+  );
+};
+
+export default About;
